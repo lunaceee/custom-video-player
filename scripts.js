950a42ffefcf0a2fc76adc8dc714f0860a4c14ce
lunaceee <lunaceee@gmail.com>
2020-02-16 16:58:54 -0800

    📽

---
+++ scripts.js
@@ -0,0 +1,75 @@
+// Get elements
+const player = document.querySelector(".player");
+const video = player.querySelector(".viewer");
+const progress = player.querySelector(".progress");
+const progressBar = player.querySelector(".progress__filled");
+const toggle = player.querySelector(".toggle");
+const skipButtons = player.querySelectorAll("[data-skip");
+const ranges = player.querySelectorAll(".player__slider");
+const fullscreenIcon = player.querySelector(".fullscreen-icon");
+const unfullscreenIcon = player.querySelector(".unfullscreen-icon");
+
+// Build out functions
+function togglePlay() {
+  const method = video.paused ? "play" : "pause";
+  video[method]();
+}
+
+function updateButton() {
+  const icon = this.paused ? "▶︎" : "❚ ❚";
+  toggle.textContent = icon;
+}
+
+function skip() {
+  console.log(this.dataset.skip);
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+  video[this.name] = this.value;
+}
+
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+}
+
+function show(el) {
+  el.style = "display: block";
+}
+function hide(el) {
+  el.style = "display: none";
+}
+
+function enableFullscreen() {
+  player.requestFullscreen();
+  show(unfullscreenIcon);
+  hide(fullscreenIcon);
+}
+
+function exitFullscreen() {
+  document.exitFullscreen();
+  show(fullscreenIcon);
+  hide(unfullscreenIcon);
+}
+// Hook up the event listeners
+video.addEventListener("click", togglePlay);
+video.addEventListener("play", updateButton);
+video.addEventListener("pause", updateButton);
+video.addEventListener("timeupdate", handleProgress);
+toggle.addEventListener("click", togglePlay);
+skipButtons.forEach(button => button.addEventListener("click", skip));
+ranges.forEach(range => range.addEventListener("mousemove", handleRangeUpdate));
+fullscreenIcon.addEventListener("click", enableFullscreen);
+unfullscreenIcon.addEventListener("click", exitFullscreen);
+
+let mousedown = false;
+progress.addEventListener("click", scrub);
+progress.addEventListener("mousemove", e => mousedown && scrub(e));
+progress.addEventListener("mousedown", () => (mousedown = true));
+progress.addEventListener("mouseup", () => (mousedown = false));
